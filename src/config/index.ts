c288b72aa3dc642bfc2f2d4607dcc91fa3746b2b
JamesRomero2 <haymeemotoh22@gmail.com>
2023-07-09 19:37:48 +0800

    Optimize

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,3 @@
-import dotenv from 'dotenv';
-
-dotenv.config()
-
 const development = process.env.NODE_ENV !== 'production';
 
 export const server = development ? "http://localhost:3000" : 'https://jamesromero.tech'
